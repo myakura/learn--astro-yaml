d6908dd06e67f144d2b8be428a417b0a31dd0980
Masataka Yakura <masataka.yakura@gmail.com>
2021-11-24 17:47:09 +0900

    add @rollup/plugin-yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
+import yaml from '@rollup/plugin-yaml';
+
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Comment out "renderers: []" to enable Astro's default component support.
   renderers: [],
+	vite: {
+    plugins: [yaml()],
+  },
 });
